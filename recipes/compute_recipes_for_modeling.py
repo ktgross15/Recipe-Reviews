41842f1051479c4d1cca6210ebdb470225ae43a1
ktgross15 <ktgross15>
2019-06-20 08:55:18 -0400

    Recipe kgr.compute_recipes_for_modeling saved through notebook notebook editor for compute_recipes_for_modeling

--- recipes/compute_recipes_for_modeling.py
+++ recipes/compute_recipes_for_modeling.py
@@ -1,20 +1,30 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 recipes_prepared = dataiku.Dataset("recipes_prepared")
-recipes_prepared_df = recipes_prepared.get_dataframe()
+df = recipes_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['rating_40+'] = np.where(df['rating']>4.0, 1, 0)
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['rating'] = df['rating'].apply(lambda x:round(x*2)/2)
 
-recipes_for_modeling_df = recipes_prepared_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df = df[df['num_reviews']>=3]
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.info()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
-recipes_for_modeling = dataiku.Dataset("recipes_for_modeling")
-recipes_for_modeling.write_with_schema(recipes_for_modeling_df)
+df_final = dataiku.Dataset("recipes_for_modeling")
+recipes_for_modeling.write_with_schema(df)
