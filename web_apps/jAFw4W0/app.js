fdce2c976e1ce05172cae1b98544c9eea736703f
ktgross15 <katie.gross@dataiku.com>
2020-03-29 18:29:33 -0400

    Saved web app Recipes - Interactive Scoring (kgr.jAFw4W0)

--- web_apps/jAFw4W0/app.js
+++ web_apps/jAFw4W0/app.js
@@ -1,69 +1,336 @@
-/*
- * For more information, refer to the "Javascript API" documentation:
- * https://doc.dataiku.com/dss/latest/api/js/index.html
- */
+const messageContainer = document.getElementById('message');
+const clearResultsLink = document.getElementById('clear-results');
+const resultsContainer = document.getElementById('results');
+const scoringModeLink = document.getElementById('scoring-mode');
+const twoClassResultsBody = document.getElementById('two-class-table-content');
+const multiClassResultsBody = document.getElementById('multi-class-table-content');
+const regressionResultsBody = document.getElementById('regression-table-content');
+const scoringJsonForm = document.getElementById('scoring-json-form');
+const scoringForm = document.getElementById('scoring-form');
+const scoringButton = document.getElementById('scoring-button');
+const scoringTextarea = document.getElementById('scoring-textarea');
+const scoringUrl = getWebAppBackendUrl('score');
+let datasetSchemaUrl;
+let datasetSchema;
+let defaultValues;
+let formFeatures = false;
+let persistResults = dataiku.getWebAppConfig()['input_history'];
+let dateFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+};
 
-let exportButton = document.getElementById('export-button');
+let inputModelId = dataiku.getWebAppConfig()['input_model'];
+let inputDatasetName = dataiku.getWebAppConfig()['input_dataset']
 
-exportButton.addEventListener('click', function (event) {
-    let analysisName = document.getElementById('analysis-name');
-    let analysisValue = analysisName.value || '(no analysis name typed)';
-    let datasetName = document.getElementById('dataset-name');
-    let datasetValue = datasetName.value || '(no dataset chosen)';
-    let datasetColumn = document.getElementById('dataset-column');
-    let columnValue = datasetColumn.value || '(no column chosen)';
-    let rowsLimitValue = document.getElementById('rows-limit').checked;
-    let exportValue = document.querySelector('input[name="export"]:checked').value || '(no export format typed)';
+// HELPERS
 
-    alert('Now YOU should code something if you really want an export. For now, your parameters are: ' + analysisValue + ' / ' + datasetValue  + ' / ' + columnValue + ' / ' + rowsLimitValue + ' / ' + exportValue);
-    event.preventDefault();
-});
+function computeTwoClassScoringDetails(row, records) {
+    let details = `<td colspan="4"><strong>Model:</strong> ${inputModelId}
+    <strong>Dataset:</strong> ${inputDatasetName}
+    <strong>Percentile:</strong> ${row.probaPercentile}
+    <strong>Features</strong>
+    <pre><code>${JSON.stringify(records, undefined, 1)}</code></pre>
+    </td>`;
+    return details.replace(/\n/g, "<br>");
+}
 
-/* Fetch dataset sample */
-let fetchButton = document.getElementById('fetch-button');
-let datasetName = document.getElementById('dataset-name');
-let messageContainer = document.getElementById('message');
-let selectedDataset = {};
+function computeMultiClassScoringDetails(row, records) {
+    let details = `<td colspan="4"><strong>Model:</strong> ${inputModelId}
+    <strong>Dataset:</strong> ${inputDatasetName}
+    <strong>Full results</strong>
+    <pre><code>${JSON.stringify(row.probas, undefined, 1)}</code></pre>
+    <strong>Features</strong>
+    <pre><code>${JSON.stringify(records, undefined, 1)}</code></pre>
+    </td>`;
+    return details.replace(/\n/g, "<br>");
+}
 
-function displayMessage(messageText, messageClassname) {
-    messageContainer.innerHTML = messageText;
-    messageContainer.className = '';
-    if (messageClassname && messageClassname.length > 0) {
-        messageContainer.className = messageClassname;
+function computeRegressionScoringDetails(row, records) {
+    let details = `<td colspan="4"><strong>Model:</strong> ${inputModelId}
+    <strong>Dataset:</strong> ${inputDatasetName}
+    <strong>Features</strong>
+    <pre><code>${JSON.stringify(records, undefined, 1)}</code></pre>
+    </td>`;
+    return details.replace(/\n/g, "<br>");
+}
+
+function displayTwoClassResults(rows, records) {
+    resultsContainer.className += ' two-class-scoring--visible';
+
+    rows.forEach(row => {
+        let summaryRow = document.createElement('tr');
+        let detailsRow = document.createElement('tr');
+        summaryRow.classList.add('result-summary');
+        detailsRow.classList.add('result-details');
+        rowContent = `<td>${new Date().toLocaleString('en-us', dateFormatOptions)}</td>`;
+        Object.keys(row.probas).forEach((proba, index) => {
+            let domColumnValue = document.querySelector('#class-' + index);
+            domColumnValue.innerHTML = proba;
+            rowContent += `<td>${row.probas[proba]}</td>`;
+        });
+        rowContent += `<td><strong>${row.prediction}</strong></td>`;
+        summaryRow.innerHTML = rowContent;
+        detailsRow.innerHTML = computeTwoClassScoringDetails(row, records);
+        twoClassResultsBody.insertAdjacentElement('afterbegin', detailsRow);
+        twoClassResultsBody.insertAdjacentElement('afterbegin', summaryRow);
+        summaryRow.addEventListener('click', () => {
+            summaryRow.classList.toggle('result-details--visible');
+        });
+    })
+    clearResultsLink.style.display = 'block';
+}
+
+function displayMultiClassResults(rows, records) {
+    resultsContainer.className += ' multi-class-scoring--visible';
+
+    if (persistResults === false) {
+        multiClassResultsBody.innerHTML = '';
+    }
+
+    rows.forEach(row => {
+        let summaryRow = document.createElement('tr');
+        let detailsRow = document.createElement('tr');
+        summaryRow.classList.add('result-summary');
+        detailsRow.classList.add('result-details');
+        summaryRow.innerHTML = `<td>${new Date().toLocaleString('en-us', dateFormatOptions)}</td><td><strong>${row.prediction}</strong></td><td>${row.probas[row.prediction]}</td>`;
+        detailsRow.innerHTML = computeMultiClassScoringDetails(row, records);
+        multiClassResultsBody.insertAdjacentElement('afterbegin', detailsRow);
+        multiClassResultsBody.insertAdjacentElement('afterbegin', summaryRow);
+        summaryRow.addEventListener('click', () => {
+            summaryRow.classList.toggle('result-details--visible');
+        });
+    })
+    clearResultsLink.style.display = 'block';
+}
+
+function displayRegressionResults(rows, records) {
+    resultsContainer.className += ' regression-scoring--visible';
+
+    if (persistResults === false) {
+        regressionResultsBody.innerHTML = '';
+    }
+
+    rows.forEach(row => {
+        let summaryRow = document.createElement('tr');
+        let detailsRow = document.createElement('tr');
+        summaryRow.classList.add('result-summary');
+        detailsRow.classList.add('result-details');
+        summaryRow.innerHTML = `<td>${new Date().toLocaleString('en-us', dateFormatOptions)}</td><td><strong>${row.prediction}</strong></td>`;
+        detailsRow.innerHTML = computeRegressionScoringDetails(row, records);
+        regressionResultsBody.insertAdjacentElement('afterbegin', detailsRow);
+        regressionResultsBody.insertAdjacentElement('afterbegin', summaryRow);
+        summaryRow.addEventListener('click', () => {
+            summaryRow.classList.toggle('result-details--visible');
+        });
+    })
+    clearResultsLink.style.display = 'block';
+}
+
+function displayResults(predictionData, records) {
+    resultsContainer.className += ' results-table--visible';
+    clearError();
+
+    if (predictionData.regression) {
+        displayRegressionResults(predictionData.regression, records);
+    } else if (predictionData.classification) {
+        if (predictionData.classification[0] && predictionData.classification[0].probaPercentile) {
+            displayTwoClassResults(predictionData.classification, records);
+        } else {
+            displayMultiClassResults(predictionData.classification, records);
+        }
+    } // else nothing
+}
+
+function displayError() {
+    messageContainer.innerHTML = 'Please add features to score.';
+    (persistResults === false) && resultsContainer.classList.remove('results-table--visible');
+}
+
+function displayDatasetError() {
+    console.error(arguments)
+    messageContainer.innerHTML = 'The dataset schema cannot be retrieved. Please check the logs.';
+    (persistResults === false) && resultsContainer.classList.remove('results-table--visible');
+}
+
+function displayScoringError() {
+    messageContainer.innerHTML = 'Scoring failed. Please check the logs or change the inputs.';
+    (persistResults === false) && resultsContainer.classList.remove('results-table--visible');
+}
+
+function clearError() {
+    messageContainer.innerHTML = '';
+}
+
+function readForm(form) {
+    let features = {};
+    let elements = form.querySelectorAll('input, select, textarea');
+    Array.prototype.slice.call(elements).forEach(element => {
+        if (element.type && element.type === 'checkbox') {
+            features[element.name] = [element.checked];
+        } else {
+            features[element.name] = [element.value == 'null' ? null : element.value];
+        }
+    });
+    return features;
+}
+
+function score(event) {
+    event && event.preventDefault();
+
+    let records;
+    if (formFeatures) {
+        records = readForm(scoringForm);
+    } else {
+        records = JSON.parse(scoringTextarea.value);
+    }
+    if (!records) {
+        displayError();
+        return false;
+    }
+
+    fetch(scoringUrl, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({records: records})
+    })
+        .then(response => response.json())
+        .then(response => displayResults(response.prediction, records))
+        .catch(displayScoringError);
+}
+
+
+function updateTextarea() {
+    scoringTextarea.value = '';
+    scoringTextarea.value += '{\n';
+    datasetSchema.forEach((feature, index) => {
+        let val = defaultValues[feature.name][0];
+        if (val == null) {
+            scoringTextarea.value += ` "${feature.name}": [null],\n`;
+        } else {
+            scoringTextarea.value += ` "${feature.name}": ["${defaultValues[feature.name][0]}"],\n`;
+        }
+    });
+    scoringTextarea.value = scoringTextarea.value.substring(0, scoringTextarea.value.length - 2) + '\n}';
+}
+
+
+function updateForm() {
+    let content = '';
+    for (feature of datasetSchema) {
+        let value = defaultValues[feature.name.toString()][0];
+        switch (feature.computedType) {
+            case 'double':
+            case 'bigint':
+                content += `<div class="field"><label for="${feature.name}">${feature.name}</label><input type="number" step="any" name="${feature.name}" value="${value}"></input>`;
+                break;
+
+            case 'string':
+                content += `<div class="field"><label for="${feature.name}">${feature.name}</label><input type="text" name="${feature.name}" value="${value}"></input></div>`;
+                break;
+
+            case 'boolean':
+                let checked = value === true;
+                if (checked === true) {
+                    content += `<div class="field"><label for="${feature.name}">${feature.name}</label><input checked type="checkbox" tabindex="0" name="${feature.name}" value="${feature.name}"></input></div>`;
+                } else {
+                    content += `<div class="field"><label for="${feature.name}">${feature.name}</label><input type="checkbox" tabindex="0" name="${feature.name}" value="${feature.name}"></input></div>`;
+                }
+
+                break;
+            case 'categorical':
+                let options = '';
+                if (!feature.values.includes(value)) {
+                    feature.values.push(value);
+                }
+                for (featureValue of feature.values) {
+                    if (featureValue === value) {
+                        options += ` <option value="${value}" selected>${value}</option> `;
+                    } else {
+                        options += ` <option value="${featureValue}">${featureValue}</option> `;
+                    }
+                }
+                content += `<div class="field"><label for="${feature.name}">${feature.name}</label><div class="select"><select tabindex="0" name="${feature.name}">${options}</select></div></div>`;
+                break;
+        }
+    }
+
+    scoringForm.innerHTML = content;
+    Array.prototype.slice.call(scoringForm.querySelectorAll('input, select, textarea')).forEach(element => {
+        element.addEventListener('change', updateDefaultValuesFromForm);
+    });
+}
+
+
+function updateSample() {
+    datasetSchemaUrl = getWebAppBackendUrl('get-dataset-schema') + '?dataset_name=' + inputDatasetName;
+    scoringTextarea.value = '';
+    fetch(datasetSchemaUrl).then(response => response.json()).then(response => {
+        datasetSchema = response.schema;
+        defaultValues = JSON.parse(response.defaultValues);
+        clearError();
+
+        updateTextarea();
+        updateForm();
+    }).catch(displayDatasetError);
+}
+
+function updateSampleFromJson() {
+    defaultValues = JSON.parse(scoringTextarea.value);
+    updateForm();
+}
+
+
+function updateDefaultValuesFromForm() {
+    defaultValues = readForm(scoringForm);
+    updateTextarea();
+}
+
+
+function switchScoringMode() {
+    formFeatures = !formFeatures;
+    if (formFeatures) {
+        scoringModeLink.innerHTML = 'Switch to JSON view';
+        scoringJsonForm.style.display = 'none';
+        scoringForm.style.display = 'block';
+    } else {
+        scoringModeLink.innerHTML = 'Switch to form view';
+        scoringJsonForm.style.display = 'block';
+        scoringForm.style.display = 'none';
     }
 }
 
-function clearMessage() {
-    displayMessage('');
+
+function clearResults() {
+    twoClassResultsBody.innerHTML = '';
+    multiClassResultsBody.innerHTML = '';
+    regressionResultsBody.innerHTML = '';
+    resultsContainer.classList.remove('results-table--visible');
+    clearResultsLink.style.display = 'none';
 }
 
-function displayFailure() {
-    displayMessage('The dataset cannot be retrieved. Please check the dataset name or the API Key\'s permissions in the "Settings" tab of the webapp.', 'error-message');
-}
 
-function displayDataFrame(dataFrame) {
-    let columnsNames = dataFrame.getColumnNames();
-    let line = '------------------------------';
-    let text = selectedDataset.name + '\n'
-        + line + '\n'
-        + dataFrame.getNbRows() + ' Rows\n'
-        + columnsNames.length + ' Columns\n'
-        + '\n' + line + '\n'
-        + 'Columns names: \n';
-    columnsNames.forEach(function(columnName) {
-        text += columnName + ', ';
-    });
-    displayMessage(text);
-}
+// Attach listeners
+scoringModeLink.addEventListener('click', switchScoringMode);
+scoringButton.addEventListener('click', score);
+scoringTextarea.addEventListener('change', updateSampleFromJson);
+clearResultsLink.addEventListener('click', clearResults);
 
-fetchButton.addEventListener('click', function(event) {
-    clearMessage();
-    selectedDataset.name = document.getElementById('dataset-to-fetch').value;
-    dataiku.fetch(selectedDataset.name, function(dataFrame) {
-        selectedDataset.dataFrame = dataFrame;
-        displayDataFrame(dataFrame);
-    }, function() {
-        displayFailure();
-    });
-    return false;
+
+if (inputModelId && inputDatasetName) {
+    updateSample()
+    switchScoringMode();
+} else {
+    document.querySelector('body').classList.add('no-inputs');
+}
+$.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
+    console.log('Received data from backend', data)
+    const output = $('<pre />').text('Backend reply: ' + JSON.stringify(data));
+    $('body').append(output)
 });
