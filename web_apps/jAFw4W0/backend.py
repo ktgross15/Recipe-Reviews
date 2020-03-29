1e8e7e262c190e8bd9151d95fa73faa18f6e1746
ktgross15 <katie.gross@dataiku.com>
2020-03-29 18:30:02 -0400

    Saved web app Recipes - Interactive Scoring (kgr.jAFw4W0)

--- web_apps/jAFw4W0/backend.py
+++ web_apps/jAFw4W0/backend.py
@@ -1,22 +1,48 @@
+from dataiku.customwebapp import *
 import dataiku
 import pandas as pd
+from dataiku.apinode.predict.server import handle_predict
 from flask import request
+import json
 
 
-# Example:
-# As the Python webapp backend is a Flask app, refer to the Flask
-# documentation for more information about how to adapt this
-# example to your needs.
-# From JavaScript, you can access the defined endpoints using
-# getWebAppBackendUrl('first_api_call')
+SAMPLE_SIZE = 10000
+THRESHOLD_CARDINALITY = 100
 
-@app.route('/first_api_call')
-def first_call():
-    max_rows = request.args.get('max_rows') if 'max_rows' in request.args else 500
+model_id = get_webapp_config().get("input_model")
+if model_id is None:
+    raise RuntimeError("Model not specified")
+model = dataiku.Model(model_id)
+predictor = model.get_predictor()
 
-    mydataset = dataiku.Dataset("REPLACE_WITH_YOUR_DATASET_NAME")
-    mydataset_df = mydataset.get_dataframe(sampling='head', limit=max_rows)
+def get_categoricals(dataset, schema):
+    """
+    Detects low cardinality features and consider them as categoricals
+    Returns the dataset schema enriched with the values of its categorical features
+    """
+    df = dataset.get_dataframe(limit=SAMPLE_SIZE)
+    for column in schema:
+        values = df[column['name']].unique()
+        if len(values) < THRESHOLD_CARDINALITY:
+            column['computedType'] = 'categorical'
+            column['values'] = [None if pd.isnull(x) else x for x in values]
+        else:
+            column['computedType'] = column['type']
+    return schema
 
-    # Pandas dataFrames are not directly JSON serializable, use to_json()
-    data = mydataset_df.to_json()
-    return json.dumps({"status": "ok", "data": data})
+
+@app.route('/get-dataset-schema')
+def get_dataset_schema():
+    dataset = dataiku.Dataset(request.args.get('dataset_name'))
+    schema = dataset.read_schema()
+    default_values = dataset.get_dataframe(limit=1)
+    schema = get_categoricals(dataset, schema)
+    return json.dumps({ "schema": schema, "defaultValues": default_values.to_json() })
+
+
+@app.route('/score', methods=['POST'])
+def score():
+    payload = request.get_json()
+    data = { "records": payload['records'], "schema": predictor.params.schema }
+    prediction = handle_predict(predictor, data)
+    return json.dumps({ "prediction": prediction })
