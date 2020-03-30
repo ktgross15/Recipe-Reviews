c5992e41a702bc41398c7dd4ea9d5ad7f70f4eaa
ktgross15 <katie.gross@dataiku.com>
2020-03-30 12:27:59 -0400

    Saved web app Recipes - Interactive Scoring (kgr.jAFw4W0)

--- web_apps/jAFw4W0/app.js
+++ web_apps/jAFw4W0/app.js
@@ -13,7 +13,7 @@ const scoringUrl = getWebAppBackendUrl('score');
 let datasetSchemaUrl;
 let datasetSchema;
 let defaultValues;
-// let formFeatures = false;
+let formFeatures = false;
 let dateFormatOptions = {
     year: 'numeric',
     month: 'short',
